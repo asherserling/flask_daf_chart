2afb356a94be56e298e61690bb68e6b38801823e
Asher Serling <asherserling@gmail.com>
2020-08-27 13:10:14 +0300

    average function

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -16,6 +17,16 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+
+
+
+
 
 class Masechta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
